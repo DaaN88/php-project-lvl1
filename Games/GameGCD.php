701cad9ef4b8ec13f156a47df6ccb888293d51f9
DaaN88 <ant-shvedov@yandex.ru>
2020-06-15 21:31:17 +0400

    stage 7 php project lvl-1; edit in Readme.md lbl on lvl

---
+++ Games/GameGCD.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BrainGames\Games\GameGCD;
+
+use function BrainGames\Engine\Engine\startEngineGame;
+
+function startGCDGame()
+{
+    $expressionsAndAnswers = [];
+
+    for ($i = 0; $i < 3; $i++) {
+        $firstNumber = mt_rand(1, 1000);
+        $secondNumber = mt_rand(1, 1000);
+
+        $stringExpression = $firstNumber . " " . $secondNumber;
+
+        $expressionsAndAnswers[$i] = [
+            $stringExpression =>
+            findGCD(
+                $firstNumber,
+                $secondNumber
+            )
+        ];
+    }
+
+    startEngineGame(
+        'What is the result of the expression?',
+        $expressionsAndAnswers
+    );
+}
+
+function findGCD($transmittedFirstNumber, $transmittedSecondNumber)
+{
+    while ($transmittedSecondNumber !== 0) {
+        $buffer = $transmittedFirstNumber % $transmittedSecondNumber;
+        $transmittedFirstNumber = $transmittedSecondNumber;
+        $transmittedSecondNumber = $buffer;
+    }
+    return $transmittedFirstNumber;
+}
