7f4e78925563b6f08b3ae275bc165850286ea742
DaaN88 <ant-shvedov@yandex.ru>
2020-06-18 15:08:48 +0400

    moved dir Engine & Games in dir src

---
+++ src/Games/GameCalculator.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BrainGames\Games\GameCalculator;
+
+use function BrainGames\Engine\Engine\startEngineGame;
+
+function startCalcGame()
+{
+    $expressionsAndAnswers = [];
+
+    for ($i = 0; $i < 3; $i++) {
+        $firstNumber = mt_rand(1, 1000);
+        $secondNumber = mt_rand(1, 1000);
+
+        $arrayOperations = ['+', '-', '*'];
+        $operations = $arrayOperations[mt_rand(0, count($arrayOperations) - 1)];
+
+        $question = $firstNumber . " " . $operations . " " . $secondNumber;
+
+        $expressionsAndAnswers[$i] = [
+                                        "stringWithQuestion" => $question,
+                                        "stringWithAnswer" => evaluateExpression(
+                                            $firstNumber,
+                                            $operations,
+                                            $secondNumber
+                                        )
+                                     ];
+    }
+
+    startEngineGame(
+        'What is the result of the expression?',
+        $expressionsAndAnswers
+    );
+}
+
+function evaluateExpression($transmittedFirstNumber, $transmittedOperations, $transmittedSecondNumber)
+{
+    $result = 0;
+
+    switch ($transmittedOperations) {
+        case '+':
+            $result = $transmittedFirstNumber + $transmittedSecondNumber;
+            break;
+        case '-':
+            $result = $transmittedFirstNumber - $transmittedSecondNumber;
+            break;
+        case '*':
+            $result = $transmittedFirstNumber * $transmittedSecondNumber;
+            break;
+    }
+
+    return (int)$result;
+}
